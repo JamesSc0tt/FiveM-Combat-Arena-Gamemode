3f6e5ffb7f6af25c8bc1082c3a282e2d6e1fdfbf
JamesSc0tt <jamesscott.js95@gmail.com>
2021-07-21 22:37:25 +0100

    [fca-round] handle death

---
+++ [fca]/fca-round/sv_round.lua
@@ -0,0 +1,13 @@
+local round_data = {}
+
+function handle_death()
+
+end
+
+AddEventHandler('baseevents:onPlayerKilled', function(player, killer, reason)
+
+end)
+
+AddEventHandler('baseevents:onPlayerDied', function(killedBy, pos)
+
+end)
