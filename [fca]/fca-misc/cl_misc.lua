86894bbca40c300f9d760d581704341bf499ec25
JamesSc0tt <jamesscott.js95@gmail.com>
2021-07-21 21:46:19 +0100

    [fca-misc] draw text function

---
+++ [fca]/fca-misc/cl_misc.lua
@@ -0,0 +1,13 @@
+function drawTxt(x, y, width, height, scale, text, r, g, b, a, outline)
+    SetTextFont(4)
+    SetTextProportional(0)
+    SetTextScale(scale, scale)
+    SetTextColour(r, g, b, a)
+    SetTextDropShadow(0, 0, 0, 0, 255)
+    SetTextEdge(2, 0, 0, 0, 255)
+    SetTextDropShadow()
+    SetTextOutline()
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(x - width / 2, y - height / 2 + 0.005)
+end
--- [fca]/fca-misc/fxmanifest.lua
+++ [fca]/fca-misc/fxmanifest.lua
@@ -3,3 +3,5 @@ game 'gta5'
 
 client_script 'cl_misc.lua'
 server_script 'sv_misc.lua'
+
+export 'drawTxt'
