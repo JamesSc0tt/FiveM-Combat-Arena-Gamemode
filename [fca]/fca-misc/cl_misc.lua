0d1f3b552fce82466102b6d386def27f410e54f6
JamesSc0tt <jamesscott.js95@gmail.com>
2021-07-22 23:50:06 +0100

    [fca-misc] temp show text on screen

--- [fca]/fca-misc/cl_misc.lua
+++ [fca]/fca-misc/cl_misc.lua
@@ -9,5 +9,30 @@ function drawTxt(x, y, width, height, scale, text, r, g, b, a, outline)
     SetTextOutline()
     SetTextEntry("STRING")
     AddTextComponentString(text)
+    SetTextCentre(true)
     DrawText(x - width / 2, y - height / 2 + 0.005)
 end
+
+RegisterNetEvent('fca-misc:text:LG')
+AddEventHandler('fca-misc:text:LG', function(text, color, seconds)
+    text=string.upper(text)
+    Citizen.CreateThread(function()
+        local destroy = GetGameTimer() + (seconds * 1000)
+        while GetGameTimer() < destroy do
+            Citizen.Wait(0)
+            exports['fca-misc']:drawTxt(1.0, 0.8, 1.0, 1.0, 2.0, text, color[1], color[2], color[3], 255)
+        end
+    end)
+end)
+
+RegisterNetEvent('fca-misc:text:SM')
+AddEventHandler('fca-misc:text:SM', function(text, color, seconds)
+    text=string.upper(text)
+    Citizen.CreateThread(function()
+        local destroy = GetGameTimer() + (seconds * 1000)
+        while GetGameTimer() < destroy do
+            Citizen.Wait(0)
+            exports['fca-misc']:drawTxt(1.0, 0.93, 1.0, 1.0, 0.8, text, color[1], color[2], color[3], 255)
+        end
+    end)
+end)
