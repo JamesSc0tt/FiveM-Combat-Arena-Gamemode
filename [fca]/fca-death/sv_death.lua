4aa0c945deb4d648b12d11983bb743fa64d69cb2
JamesSc0tt <jamesscott.js95@gmail.com>
2021-07-24 16:43:16 +0100

    death alerts discord / tell fca-round

--- [fca]/fca-death/sv_death.lua
+++ [fca]/fca-death/sv_death.lua
@@ -10,5 +10,25 @@ AddEventHandler('fca-death:report', function(info)
 				print('info['..tostring(k)..'] = '..tostring(v))
 			end
 		end
+
+		-- death messages
+
+		local midstring = ''
+
+		local killedstrings = {'brutally murdered', 'ended'}
+		local suicidestrings = {'committed suicide', 'took the easy way out'}
+
+		if not info[1].id  then
+			midstring = suicidestrings[math.random(1,#suicidestrings)]
+			exports['fca-discord']:AddDiscordLog('player', '**'..GetPlayerName(source)..'** '..midstring)
+			TriggerClientEvent('FeedM:showNotification', -1, '~b~'..GetPlayerName(source)..'~w~ '..midstring)
+			TriggerEvent('fca-round:death', source, false)
+		else
+			-- new midstring
+			midstring = killedstrings[math.random(1,#killedstrings)]
+			exports['fca-discord']:AddDiscordLog('player', '**'..info[1].name..'** '..midstring..' **'..GetPlayerName(source)..'**')
+			TriggerClientEvent('FeedM:showNotification', -1, '~b~'..info[1].name..'~w~ '..midstring..' ~b~'..GetPlayerName(source))
+			TriggerEvent('fca-round:death', source, info[1].id)
+		end
 	end
 end)
