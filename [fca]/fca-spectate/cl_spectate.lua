67afe4631ce0c999604c1e26f3dfd41a64380bfa
JamesSc0tt <jamesscott.js95@gmail.com>
2021-07-24 19:14:05 +0100

    [fca-spectate] remove debug text print

--- [fca]/fca-spectate/cl_spectate.lua
+++ [fca]/fca-spectate/cl_spectate.lua
@@ -81,6 +81,7 @@ end)
 RegisterNetEvent('fca-spectate:spectatePlayer')
 AddEventHandler('fca-spectate:spectatePlayer', function(id, coords)
     if coords == nil then return end
+    SetEntityVisible(GetPlayerPed(-1), false)
     SetEntityCoords(GetPlayerPed(-1), coords)
 
 
@@ -157,43 +158,6 @@ Citizen.CreateThread(function()
 			else
 				print'nocam?'
 			end
-		end
-	end
-end)
-
-local function DrawText3D(x,y,z, text, r,g,b) 
-    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
-    local px,py,pz=table.unpack(GetGameplayCamCoords())
-    local dist = #(vector3(px,py,pz)-vector3(x,y,z))
-    local scale = (1/dist)*2
-    local fov = (1/GetGameplayCamFov())*100
-    local scale = scale*fov
-    if onScreen then
-        if not useCustomScale then
-            SetTextScale(0.0*scale, 0.55*scale)
-        else 
-            SetTextScale(0.0*scale, customScale)
-        end
-        SetTextFont(0)
-        SetTextProportional(1)
-        SetTextColour(r, g, b, 255)
-        SetTextDropshadow(0, 0, 0, 0, 255)
-        SetTextEdge(2, 0, 0, 0, 150)
-        SetTextDropShadow()
-        SetTextOutline()
-        SetTextEntry("STRING")
-        SetTextCentre(1)
-        AddTextComponentString(text)
-        DrawText(_x,_y)
-    end
-end
-
-Citizen.CreateThread(function()
-	while true do Citizen.Wait(0)
-		for _, id in ipairs(GetActivePlayers()) do
-			SetMpGamerTagVisibility(GetPlayerPed(id), GetPlayerServerId( id ) .. "", false, false, "", false )
-			local loc = GetEntityCoords(GetPlayerPed(id))
-			DrawText3D(loc.x,loc.y,loc.z, GetPlayerServerId( id ), 255,0,0) 
 		end
 	end
 end)
