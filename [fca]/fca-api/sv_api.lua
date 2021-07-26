fa865e3a95eaeb358556b3ec06ddcd7210f8822f
JamesSc0tt <jamesscott.js95@gmail.com>
2021-07-26 21:33:01 +0100

    [fca-api] main interaction with api endpoints

---
+++ [fca]/fca-api/sv_api.lua
@@ -0,0 +1,40 @@
+local API_URL = 'https://combatarena.co/apiv1'
+local DEBUG_MODE = false
+
+function api_request(endpoint, args, postdata, headers)
+	local response = false
+	if string.sub(endpoint, 1, 1) ~= '/' then
+		endpoint = '/'..endpoint
+	end
+	if not headers then
+		headers = {}
+	end
+	local url = API_URL..endpoint
+	if (postdata) then
+		PerformHttpRequest(url, function(errorCode, resultData, resultHeaders)
+			response['errorCode'] = errorCode
+			response['resultData'] = resultData
+			response['resultHeaders'] = resultHeaders
+
+			if DEBUG_MODE then
+				print('fca-api : response')
+				print(' - errorCode: '..errorCode)
+				print(' - resultData: '..resultData)
+				print(' - resultHeaders: '..resultHeaders)
+			end
+		end, 'POST', postdata, headers)
+	else
+		PerformHttpRequest(url, function(errorCode, resultData, resultHeaders)
+			response['errorCode'] = errorCode
+			response['resultData'] = resultData
+			response['resultHeaders'] = resultHeaders
+
+			if DEBUG_MODE then
+				print('fca-api : response')
+				print(' - errorCode: '..errorCode)
+				print(' - resultData: '..resultData)
+				print(' - resultHeaders: '..resultHeaders)
+			end
+		end, 'GET', {}, headers)
+	end
+end
